80af8b70de01d4509074c231592b2893a79ed553
Vladimir <fargus9595@gmail.com>
2020-10-13 16:48:18 +0300

    Chameleon

---
+++ src/main/java/ru/aplana/autotests/steps/CustomDebugSteps.java
@@ -0,0 +1,17 @@
+package ru.aplana.autotests.steps;
+
+import com.aplana.chameleon.steps.AbstractSteps;
+import io.qameta.allure.Step;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomDebugSteps extends AbstractSteps {
+
+    public CustomDebugSteps() {
+    }
+
+    @Step("debug")
+    public void stepDebug() {
+        System.out.println("debug");
+    }
+}
---
+++ src/main/java/ru/aplana/autotests/steps/CustomDebugStorySteps.java
@@ -0,0 +1,20 @@
+package ru.aplana.autotests.steps;
+
+import com.aplana.chameleon.steps.AbstractSteps;
+import cucumber.api.java.ru.Когда;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomDebugStorySteps extends AbstractSteps {
+
+
+    @Autowired
+    private CustomDebugSteps customDebugSteps;
+
+
+    @Когда("^debug")
+       public void stepDebug() {
+        flow(() ->
+                customDebugSteps.stepDebug()
+        );
+    }
+}
